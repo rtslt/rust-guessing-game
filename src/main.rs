4e45e4ec8a0bfaf4f288ff51ad0a6789f44508c2
RattasatL <3gthebreak@gmail.com>
2023-03-13 15:25:17 +0700

    ignore error

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use rand::Rng;
 fn main() {
     println!("Guessing the number!");
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("The secret number is {secret_number}");
+    // println!("The secret number is {secret_number}");
     
     
 
@@ -17,7 +17,10 @@ fn main() {
             .read_line(&mut input)
             .expect("Failed to read line.");
 
-        let input: u32 = input.trim().parse().expect("Input must be a number!");
+        let input: u32 = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
         println!("Your guessed: {input}");
 
         match input.cmp(&secret_number) {
